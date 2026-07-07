fb2218341f9379d069c6f848a53ea96184525a55
ctln-d <caitlindu10@gmail.com>
2026-07-06 23:52:18 -0400

    lvl 2

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,6 +4,7 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -850.0
+var can_move = true
 
 
 func _physics_process(delta: float) -> void:
@@ -17,21 +18,22 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 		animated_sprite_2d.animation = "jump"
 
-	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if can_move:
+		# Handle jump.
+		if Input.is_action_just_pressed("jump") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		# Get the input direction and handle the movement/deceleration.
+		var direction := Input.get_axis("left", "right")
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
-	
-	if direction == 1.0:
-		animated_sprite_2d.flip_h = false
-	elif direction == -1.0:
-		animated_sprite_2d.flip_h = true
+		move_and_slide()
+		
+		if direction == 1.0:
+			animated_sprite_2d.flip_h = false
+		elif direction == -1.0:
+			animated_sprite_2d.flip_h = true
 		
