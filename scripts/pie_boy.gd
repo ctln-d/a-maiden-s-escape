bb33aae199d8ddd737c3ba3f8ec527058a6e1868
ctln-d <caitlindu10@gmail.com>
2026-07-31 23:34:00 -0400

    polish + add construction scene

--- scripts/pie_boy.gd
+++ scripts/pie_boy.gd
@@ -1,10 +1,12 @@
 extends Node2D
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 signal player_died
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Dough/AnimationPlayer.play("intro")
+	$Dough/AnimationPlayer.queue("loop")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,3 +17,8 @@ func _process(_delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "Player" and body.alive:
 		emit_signal("player_died", body)
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if sprite.animation == "toss":
+		sprite.play("hit")
